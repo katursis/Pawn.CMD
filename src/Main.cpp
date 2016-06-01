de3e2aa4b3a1df5662bed6e01c50856cf3e3c0dd
urShadow <videnex@gmail.com>
2016-06-01 23:30:54 +0300

    tabbing

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,35 +17,35 @@
 #define THISCALL __thiscall
 
 const char *pattern =
-"\x83\xEC\x08"			/*sub esp,0x8*/ \
-"\x53"					/*push ebx*/ \
-"\x8B\x5C\x24\x14"		/*mov ebx,DWORD PTR [esp+0x14]*/ \
-"\x55"					/*push ebp*/ \
-"\x8B\x6C\x24\x14"		/*mov ebp,DWORD PTR [esp+0x14]*/ \
-"\x56"					/*push esi*/ \
-"\x33\xF6"				/*xor esi,esi*/ \
-"\x57"					/*push edi*/ \
-"\x8B\xF9"				/*mov edi,ecx*/ \
-"\x89\x74\x24\x10"		/*mov DWORD PTR [esp+0x10],esi*/ \
-"\x8B\x04\xB7"			/*mov eax,DWORD PTR [edi+esi*4]*/ \
-"\x85\xC0";				/*test eax,eax*/
+"\x83\xEC\x08"		/*sub esp,0x8*/ \
+"\x53"			/*push ebx*/ \
+"\x8B\x5C\x24\x14"	/*mov ebx,DWORD PTR [esp+0x14]*/ \
+"\x55"			/*push ebp*/ \
+"\x8B\x6C\x24\x14"	/*mov ebp,DWORD PTR [esp+0x14]*/ \
+"\x56"			/*push esi*/ \
+"\x33\xF6"		/*xor esi,esi*/ \
+"\x57"			/*push edi*/ \
+"\x8B\xF9"		/*mov edi,ecx*/ \
+"\x89\x74\x24\x10"	/*mov DWORD PTR [esp+0x10],esi*/ \
+"\x8B\x04\xB7"		/*mov eax,DWORD PTR [edi+esi*4]*/ \
+"\x85\xC0";		/*test eax,eax*/
 
 const char *mask = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 #else
 #define THISCALL
 
 const char *pattern =
-"\x55"							/*push ebp*/ \
-"\x89\xE5"						/*mov ebp,esp*/ \
-"\x57"							/*push edi*/ \
-"\x56"							/*push esi*/ \
-"\x53"							/*push ebx*/ \
-"\x83\xEC\x2C"					/*sub esp,0x2c*/ \
-"\x8B\x75\x08"					/*mov esi,DWORD PTR [ebp+0x8]*/ \
+"\x55"				/*push ebp*/ \
+"\x89\xE5"			/*mov ebp,esp*/ \
+"\x57"				/*push edi*/ \
+"\x56"				/*push esi*/ \
+"\x53"				/*push ebx*/ \
+"\x83\xEC\x2C"			/*sub esp,0x2c*/ \
+"\x8B\x75\x08"			/*mov esi,DWORD PTR [ebp+0x8]*/ \
 "\xC7\x45\xE4\x00\x00\x00\x00"	/*mov DWORD PTR [ebp-0x1c],0x0*/ \
-"\x8B\x7D\x10"					/*mov edi,DWORD PTR [ebp+0x10]*/ \
-"\x89\xF3"						/*mov ebx,esi*/ \
-"\xEB\x14";						/*jmp 0x2e*/
+"\x8B\x7D\x10"			/*mov edi,DWORD PTR [ebp+0x10]*/ \
+"\x89\xF3"			/*mov ebx,esi*/ \
+"\xEB\x14";			/*jmp 0x2e*/
 
 const char *mask = "xxxxxxxxxxxxxxxxxxxxxxxxxx";
 #endif
